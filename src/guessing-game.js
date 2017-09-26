bc35ed6d4664c69542ba17e59e7fc38f0361c5f1
Ivan Kirilko <Ivan Kirilko>
2017-09-26 22:18:02 +0300

    add GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.min = null;
+      this.max = null;
+      this.candidate = null;
+    }
 
     setRange(min, max) {
+      this.min = min;
+      this.max = max;
 
     }
 
     guess() {
-
+      this.candidate = Math.round((this.min  + this.max)/2);
+      return this.candidate;
     }
 
     lower() {
-
+      this.max = Math.round((this.min  + this.max)/2);
+      return this;
     }
 
     greater() {
-
+      this.min = Math.round((this.min  + this.max)/2);
+      return this;
     }
 }
 
